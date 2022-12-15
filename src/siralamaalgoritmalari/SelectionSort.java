d6e4454335951207ee933889239de60deaf57c5c
Sennur Aldemir <99283497+sennuraldemir@users.noreply.github.com>
2022-12-15 15:38:11 +0300

    Update SelectionSort.java

--- src/siralamaalgoritmalari/SelectionSort.java
+++ src/siralamaalgoritmalari/SelectionSort.java
@@ -1,1 +1,41 @@
+package siralamaalgoritmalari;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+
+    int[] dizi;
+
+    public SelectionSort(int[] d) {
+        dizi = d;
+
+    }
+
+    public void SelectionSort() {
+        int min = 0;
+        int temp = 0;
+
+        for (int i = 0; i < dizi.length; i++) {
+            min = i;
+
+            for (int j = i + 1; j < dizi.length; j++) {
+                if (dizi[j] < dizi[min]) {
+                    min = j;
+                }
+            }
+
+            if (min != i) {  // iyileştirme: min eşit değilse i ye swap işlemi yapsın
+                temp = dizi[min];
+                dizi[min] = dizi[i];
+                dizi[i] = temp;
+            }
+        }
+    }
+    // her dış döngüde dizideki en küçük bulunur
+
+    public void print() {
+        System.out.println(Arrays.toString(dizi));
+    }
+
+}
 
